52f22f8f10e35c884ece12bb7f4d9384b2f83866
ublan <uri.blan57@gmail.com>
2024-07-12 10:03:35 -0300

    Validaciones

---
+++ src/main/java/main/java/ar/edu/utn/frbb/tup/exception/ClienteNotFoundException.java
@@ -0,0 +1,8 @@
+package main.java.ar.edu.utn.frbb.tup.exception;
+
+public class ClienteNotFoundException extends Throwable {
+    public ClienteNotFoundException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/main/java/main/java/ar/edu/utn/frbb/tup/exception/CuentaNotFoundException.java
@@ -0,0 +1,8 @@
+package main.java.ar.edu.utn.frbb.tup.exception;
+
+
+public class CuentaNotFoundException extends Throwable  {
+    public CuentaNotFoundException(String message) {
+        super(message);
+    }
+}
